d0c2c843c7a3cf6b801af830fc8ce4a3c4186e3e
Christian Trabold <christian@datacater.io>
2022-01-11 17:21:29 +0100

    feat: introduce macOS default settings
    This file contains a bunch of default settings that we recommend to make
    working with the OS more productive.
    
    Please note that execution of this script is preventend for now with
    `exit 0`. Remove this line to make this part of the initial setup.
    
    Alternaively you can also run the script manually whenever you want.
    
    Inspired by https://mths.be/macos

---
+++ hooks/post-up/mac-osx-settings.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# Configures macOS system & application settings
+#
+# Inspired by https://mths.be/macos
+##
+# REFERENCES
+# - https://www.defaults-write.com/
+# - https://git.herrbischoff.com/awesome-macos-command-line/about/
+if [[ $OSTYPE == darwin* ]]; then
+  echo "INFO Setup macOS..."
+  echo "Disabled until macOS 12.1 is fully supported."
+  exit 0
+else
+  echo "WARN No Mac OS detected. Exiting..."
+  exit 1
+fi
+
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# Finder                                                                      #
+###############################################################################
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `glyv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Disable the warning before emptying the Trash
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "Activity Monitor" \
+	"Address Book" \
+	"Calendar" \
+	"cfprefsd" \
+	"Contacts" \
+	"Dock" \
+	"Finder" \
+	"Safari" \
+	"SystemUIServer" \
+	"Terminal" \
+	"iCal"; do
+	killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
